0b9fa610472cda57cb2de1056f6b9695ea68c111
Timur0204 <timur02042008@gmail.com>
2023-12-06 22:04:54 +0200

    add that too

--- assets/js/add.js
+++ assets/js/add.js
@@ -2,11 +2,15 @@
    let roundsLeft = 10;
    let secretNumber = gen();
    
+   const oneImage = document.getElementById("oneImage");
+   const twoImage = document.getElementById("twoImage");
+   const threeImage = document.getElementById("threeImage");
+   
    function gen() {
-      let n = Math.random();
-      n = Math.floor(n * 100) + 1;
-      return n;
-}
+       let n = Math.random();
+       n = Math.floor(n * 100) + 1;
+       return n;
+   }
    
    function game() {
        let unknownNumber = +document.querySelector("#unknowNumber").value;
@@ -18,22 +22,26 @@
                if (unknownNumber === secretNumber) {
                    result.innerHTML = `Вы выиграли, это действительно число ${secretNumber}`;
                    roundsLeft = 0;
+                   oneImage.classList.add("hidden");
+                   twoImage.classList.remove("hidden");
                } else {
                    roundsLeft--;
                    if (roundsLeft === 0) {
                        result.innerHTML = `Вы проиграли, это было число ${secretNumber}`;
                        roundsElement.innerText = `Осталось попыток: 0`;
+                       oneImage.classList.add("hidden");
+                       threeImage.classList.remove("hidden");
                    } else {
-                     if (unknownNumber < secretNumber) {
-                         result.innerHTML = `Число ${unknownNumber} меньше моего`;
-                     } else {
-                         result.innerHTML = `Число ${unknownNumber} больше моего`;
-                     }
-                     roundsElement.innerText = `Осталось попыток: ${roundsLeft}`;
-                 }
+                       if (unknownNumber < secretNumber) {
+                           result.innerHTML = `Число ${unknownNumber} меньше моего`;
+                       } else {
+                           result.innerHTML = `Число ${unknownNumber} больше моего`;
+                       }
+                       roundsElement.innerText = `Осталось попыток: ${roundsLeft}`;
+                   }
                }
            } else {
                result.innerHTML = 'Введите корректные данные (число от 1 до 100)';
            }
-       } 
+       }
    }
